b548edbdcf599da1d21e67a3c5147a1dbce003e1
Emiliano-GADea <emigadeab@gmail.com>
2025-06-20 16:39:23 +0000

    la

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-int main()
-{
-char* str1 = "Programmings is like building a multilingual puzzle    "
-	;
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints'
+ * @param void
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char *str1 = "Programmings is like building a multilingual puzzle";
 puts(str1);
-return 0;
+return (0);
 }
