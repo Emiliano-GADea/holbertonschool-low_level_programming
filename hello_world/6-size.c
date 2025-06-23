634f1785b4a7ab241f4b3deeb97f80c9098c79e3
Emiliano-GADea <emigadeab@gmail.com>
2025-06-23 20:22:47 +0000

    mk

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-	printf("Size of a char: %zu byte(s)\n", sizeof(d));
+		printf("Size of a char: %zu byte(s)\n", sizeof(d));
 	printf("Size of an int: %zu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
