7316340ae68361a57bfb7f03aeed3f5e0a0f8d3e
Emiliano-GADea <emigadeab@gmail.com>
2025-06-25 18:43:38 +0000

    lp

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	
+	char letra;
+	for(letra = 'a'; letra <= 'z'; letra++){
+		putchar(letra);
+	}
+	char ses;
+	for(ses = 'a'; ses <= 'z'; ses++){
+		char lose = toupper(ses);
+		putchar(lose);
+	}
+	putchar('\n');
+	return (0);
+}	
