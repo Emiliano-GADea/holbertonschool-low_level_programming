bc96e3c3ef391e62cc7a0624a95a71e469fa20c8
Emiliano-GADea <emigadeab@gmail.com>
2025-06-27 13:18:14 +0000

    lk

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description - Top text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lel;
+
+	for (lel = '0'; lel <= '9'; lel++)
+	{
+		putchar(lel);
+	}
+	putchar ('\n');
+	return (0);
+}
