48c001551c5ad40298264e3f919bafe838dc7283
Emiliano-GADea <emigadeab@gmail.com>
2025-06-27 18:17:02 +0000

    m

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Anashel
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
