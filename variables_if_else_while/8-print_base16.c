e29188bbfb1dd1fd18a548dd096a186831793095
Emiliano-GADea <emigadeab@gmail.com>
2025-06-27 13:55:37 +0000

    ml

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Niggercode
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tic, toc;
+
+	for (tic = '0'; tic <= '9'; tic++)
+	{
+		putchar(tic);
+	}
+	for (toc = 'a'; toc <= 'f'; toc++)
+	{
+		putchar(toc);
+	}
+	putchar('\n');
+	return (0);
+}
