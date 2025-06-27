a715b1cfa17552056641532e0d06be78cb77d5b2
Emiliano-GADea <emigadeab@gmail.com>
2025-06-27 16:33:35 +0000

    mk

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - The man who sold the world
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_");
+	printf("putchar\n");
+	return (0);
+}
