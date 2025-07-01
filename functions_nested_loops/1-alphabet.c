96a75202ee82b0efedef534ce60d3257130f3527
Emiliano-GADea <emigadeab@gmail.com>
2025-07-01 13:56:25 +0000

    mj

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 /**
+ * print_alphabet - Entry point
  *
- * main - Entry point
+ * Description: 'Prints the alphabet in lowercase'
  *
- * Description - SooS
- *
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char nig;
-	
-	for(nig = 'a'; nig <= 'z'; nig++)
+
+	for (nig = 'a'; nig <= 'z'; nig++)
 	{
 		_putchar(nig);
 	}
 	_putchar('\n');
-	return (0);
 }
