d96240fe8c8cd76c760714c88a17f055422f6b19
Emiliano-GADea <emigadeab@gmail.com>
2025-07-02 14:55:07 +0000

    ml

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include <ctype.h>
 #include "main.h"
 /**
  * print_sign - Entry point
- *
+ *@n: is an int
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
-{}
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+	return (0);
+}
