e60b06ec9b48e590944db764064eb9b4192f25ca
Emiliano-GADea <emigadeab@gmail.com>
2025-07-03 17:28:11 +0000

    m

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * jack_bauer - Entry point
- *
+ * Return: void
  */
 void jack_bauer(void)
 {
 	int sel;
 	int jet;
 
-	for (sel = 0; sel <= 24; sel++)
+	for (sel = 0; sel < 24; sel++)
 	{
-		for (jet = 0; jet <= 59; jet++)
+		for (jet = 0; jet < 60; jet++)
 		{
 			_putchar((sel / 10) + '0');
 			_putchar((sel % 10) + '0');
