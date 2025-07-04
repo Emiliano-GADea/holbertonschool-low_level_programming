cdc26848a08d43e51f0e12573447af2c17a8eecc
Emiliano-GADea <emigadeab@gmail.com>
2025-07-04 16:17:58 +0000

    mj

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * @n: is an int
+ * @c: is an int
+ * Return: Always 0 (Success)
+ */
+int add(int n, int c)
+{
+	return (n + c);
+}
