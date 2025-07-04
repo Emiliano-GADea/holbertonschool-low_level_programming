c3601f0ba802b77197c32a8f8a8f7bc2ac90c84a
Emiliano-GADea <emigadeab@gmail.com>
2025-07-04 14:39:43 +0000

    ml

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,6 +1,38 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * time_tables - Entry point
+ *
  * Return: void
  */
-
+void times_table(void)
+{
+	int sas, lel, lol;
+	
+	for(sas = 0; sas <= 9; sas++)
+	{
+		for(lel = 0; lel <= 9; lel++)
+		{
+			lol = sas * lel;
+			if(lel != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if(lol < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			if(lol >= 10)
+			{
+				_putchar((lol / 10) + '0');
+				_putchar((lol % 10) + '0');
+			}
+			else
+			{
+				_putchar(lol + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
