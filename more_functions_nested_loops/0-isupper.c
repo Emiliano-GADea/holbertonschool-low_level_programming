037fa1a37d8539ad95670474f28c866469449229
Emiliano-GADea <emigadeab@gmail.com>
2025-07-08 17:31:53 +0000

    mk

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -9,14 +9,13 @@
  */
 int _isupper(int c)
 {
-	if (c == 'A')
+	if (c == 'A' && c <= 'Z')
 	{
+		c++;
 		return (1);
 	}
-	else if (c == 'a')
+	else
 	{
 		return (0);
 	}
-	putchar('\n');
-	return (0);
 }
