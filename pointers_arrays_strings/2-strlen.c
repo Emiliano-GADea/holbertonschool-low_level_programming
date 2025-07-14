e7c2596701a4cb40c6a79c415f8fcd56fdfa23ba
Emiliano-GADea <emigadeab@gmail.com>
2025-07-14 20:41:42 +0000

    v

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *@s: string to evaluate
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	
+	return(i);
+}
