f790f64a2dd98bde67f5cdc8dc7443a481123738
Emiliano-GADea <emigadeab@gmail.com>
2025-07-15 15:49:14 +0000

    ml

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,4 +4,18 @@
  * @str: is a pointer
  * Return: Always 0 (Success)
  */
+void puts2(char *str)
+{
+	int len, i;
 
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
